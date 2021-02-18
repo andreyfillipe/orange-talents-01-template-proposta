bcf5776813a1ebe7be185f731f9a633b6b8d955d
Andrey Fillipe <andreyfillipe@hotmail.com>
2021-02-18 09:53:53 -0300

    Adicionado segurança na aplicação

---
+++ src/main/java/br/com/zup/proposta/config/seguranca/SecurityConfiguration.java
@@ -0,0 +1,22 @@
+package br.com.zup.proposta.config.seguranca;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+@Profile({"dev", "prod"})
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+    }
+}
